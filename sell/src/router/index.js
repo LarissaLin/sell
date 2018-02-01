f758b7085c30e116d70dca5b293bf4ab2aae20e0
林惠娟 <563064373@qq.com>
2018-02-01 08:38:24 +0800

    v1

---
+++ sell/src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import goods from '@/components/goods/goods'
+import seller from '@/components/seller/seller'
+import ratings from '@/components/ratings/ratings'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {path: '/', redirect: '/goods'},
+    {path: '/goods', component: goods},
+    {path: '/seller', component: seller},
+    {path: '/ratings', component: ratings}
+  ]
+})
